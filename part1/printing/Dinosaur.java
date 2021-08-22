2b66ac4e7527d6e477eff8841ddf21d893c11e49
abdoulaye <abdoulayeba29@gmail.com>
2021-08-22 14:30:24 +0200

    part 1

---
+++ part1/printing/Dinosaur.java
@@ -0,0 +1,6 @@
+public class Dinosaur {
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Once upon time\nthere was\na dinosaur");
+    }
+}
