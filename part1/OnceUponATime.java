0e932a3edb2ea23f0530f37f18f6d906836b3aa0
abdoulaye <abdoulayeba29@gmail.com>
2021-08-22 12:29:45 +0200

    par 1

---
+++ part1/OnceUponATime.java
@@ -0,0 +1,6 @@
+public class OnceUponATime {
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Once upon a time\nthere was\na program");
+    }
+}
