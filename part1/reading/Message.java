aa71cbdb02118aeb0eba55043b72f53c99e2c053
abdoulaye <abdoulayeba29@gmail.com>
2021-08-22 16:04:40 +0200

    new commit

---
+++ part1/reading/Message.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Message {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message: ");
+        // Read the string written by the user, and assign it
+        String message = scanner.nextLine();
+        // Print the message written by the user
+        System.out.println(message);
+
+    }
+}
