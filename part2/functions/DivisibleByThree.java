15a0a73c6f7de0bb7e35c4ea0240ad980d6c9a0a
Abdoulaye <Abdoulayeba29@gmail.com>
2021-08-30 18:11:06 +0200

    done with part 2git add . let's move on part3

---
+++ part2/functions/DivisibleByThree.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DivisibleByThree {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //int first = Integer.valueOf(scanner.nextLine());
+        //int second = Integer.valueOf(scanner.nextLine());
+
+        divisibleByThreeInRange(2, 10);
+    }
+
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i > end) {
+                break;
+            } else if (i % 3 == 0) {
+                System.out.println(i);
+            } else {
+                continue;
+            }
+        }
+    }
+}
+
