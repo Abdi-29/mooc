16a7c1d6d2b6466854b7f529d2cff36bcc206d1d
Abdoulaye <Abdoulayeba29@gmail.com>
2021-08-31 21:06:27 +0200

    new commits

---
+++ part3/array/WordListExample.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class WordListExample {
+
+    public static void main(String[] args) {
+        ArrayList<String> wordList = new ArrayList<>();
+
+        wordList.add("First");
+        System.out.println(wordList.get(0));
+    }
+}
+
